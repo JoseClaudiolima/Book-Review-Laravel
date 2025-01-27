50bb5dff0531c5f6cd8c9800d06d5e26e18ac63f
JoseClaudioLima <jose.c.lima.sp@gmail.com>
2025-01-27 17:39:16 -0300

    40. Local Query Scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -11,5 +12,14 @@ class Book extends Model
 
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title){
+        // Function's name have to start with "scope" to be under Laravel pattern, and be allowed to be used as Local Query Scopes.
+        // Also being used in php artisan tinker:
+        // \App\Models\Book::title('delectus')->get();
+
+        // Transform to sql sintax: ->toSql();
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 }
