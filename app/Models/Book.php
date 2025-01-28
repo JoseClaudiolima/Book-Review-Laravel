05611b55e2ec1590ee00f77b3a7a172ca9d0f308
JoseClaudioLima <jose.c.lima.sp@gmail.com>
2025-01-28 15:10:27 -0300

    44. Getting Books with Recent Reviews

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -23,13 +23,44 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query){
-        return $query->withCount('reviews')
-        ->orderBy('reviews_count', 'desc');
+    // public function scopePopular(Builder $query){
+    //     return $query->withCount('reviews')
+    //     ->orderBy('reviews_count', 'desc');
+    // }
+
+    public function scopePopularBetween(Builder $query, $from = null, $to = null){
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query){
-        return $query->withAvg('reviews', 'rating')
-        ->orderBy('reviews_avg_rating', 'desc');
+    public function scopeHighestRated(Builder $query, $from = null, $to = null)
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $q, $from = null, $to = null){
+        if ($from && !$to){
+            $q->where('created_at', '>=', $from);
+        } else if (!$from && $to){
+            $q->where('created_at', '<=', $to);
+        } else if ($from && $to){
+            $q->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
+    public function scopePopularPublished(Builder $query, $from = null){
+        return $query->withCount('reviews')
+        ->where('created_at', '>=', $from)
+        ->orderBy('reviews_count', 'desc');
     }
 }
